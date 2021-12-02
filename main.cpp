c41317e0bec57e4c789f73dd3f7ca6e427a77d22
James Springer <james17.springer@gmail.com>
2021-12-02 13:00:15 -0500

    File name change

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <SFML/Window.hpp>
 #include <SFML/OpenGL.hpp>
 
+#include <iostream>
+
+// create vertex shader (string)
+const std::string vertex_shader = "";
+
+// create fragment shader
+const std::string fragment_shader = "";
+
+
+
 int main() {
     sf::Window window(sf::VideoMode(800, 800), "OpenGL", sf::Style::Default, sf::ContextSettings(24, 0U, 0U, 4, 3));
     window.setVerticalSyncEnabled(true);
